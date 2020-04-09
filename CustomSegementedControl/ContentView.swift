de559e4af22d8a167c155c54b3acd4d23609ef10
Miles Johnson <miles@thoughtbot.com>
2020-04-09 19:37:38 +0200

    Initial Commit

---
+++ CustomSegementedControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomSegementedControl
+//
+//  Created by Miles Johnson on 09/04/2020.
+//  Copyright © 2020 Starship Enterprise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
