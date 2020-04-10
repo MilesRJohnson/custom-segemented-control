2224fd752ae2330899140181eb1b091c81e96ff6
Miles Johnson <miles@thoughtbot.com>
2020-04-10 19:04:36 +0200

    refine custom control and update colors

--- CustomSegementedControl/ContentView.swift
+++ CustomSegementedControl/ContentView.swift
@@ -1,21 +1,38 @@
-//
-//  ContentView.swift
-//  CustomSegementedControl
-//
-//  Created by Miles Johnson on 09/04/2020.
-//  Copyright © 2020 Starship Enterprise. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+  @State var selected = "morning"
+  
+  var body: some View {
+    CustomControl(selected: self.$selected)
+    
+  }
+}
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+      .padding()
+  }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct CustomControl : View {
+  var buttonArray = ["morning", "afternoon", "evening"]
+  @Binding var selected: String
+  var body: some View {
+    HStack(alignment: .center) {
+      ForEach(0 ..< buttonArray.count) { item in
+        Button(action: {
+          self.selected = self.buttonArray[item]
+        }) {
+          Text(self.buttonArray[item])
+            .frame(height: 125.0)
+            .frame(maxWidth: .infinity)
+        }
+        .foregroundColor(self.selected == self.buttonArray[item] ? .white : Color("grey"))
+        .background(self.selected == self.buttonArray[item] ? Color("action") : Color("action-deselected"))
+        .cornerRadius(5)
+      }
     }
+    .font(.headline)
+  }
 }
